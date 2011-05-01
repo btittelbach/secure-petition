9a57d04748ef3e7f5e979bfe82400e82760195a8
Bernhard Tittelbach <bernhard@tittelbach.org>
2011-05-01 15:33:27 +0200

    improved documentation in submit.php a bit

--- submit.php
+++ submit.php
@@ -13,6 +13,15 @@ else
   unset($_SESSION['captcha_text']);
 }
 
+## save_data_encrypted takes the Form Request and returns an associative array
+## with the following keys:
+##  "success" : Boolean : false if error occured
+##  "error"   : Integer : Error Code. One of:
+##                        NO_ERROR               : no error
+##                        ERROR_MISSING_FIELDS   : some required inputfields configured in config.php were empty
+##                        ERROR_DB_REPOST        : the submitted data was already in the database (hash already exists)
+##                        ERROR_SEND_EMAIL       : error when trying to send verification e-mail
+##  "info"    : additional information, in case of ERROR_MISSING_FIELDS it's an array specifying the empty but required inputfield names
 $save_result = save_data_encrypted($_REQUEST);
 if ($save_result["success"])
   readfile($GLOBALS['pet_html']['submit_ok'][$language]);
